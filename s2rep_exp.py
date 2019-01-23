92b2680450f43ccc8952b04f5a09b931a9f6da85
Talv <krz.krawczuk@gmail.com>
2019-01-23 21:24:52 +0100

    CV replay support finalized. ready to use.
    It also exports common section for other IBEs, but without dedicated support... yet.

--- s2rep_exp.py
+++ s2rep_exp.py
@@ -106,19 +106,19 @@ ABIL_MAP = [
 ]
 
 def decode_game_result(payload):
-    print('%d: %s' % (len(payload), payload))
     rd = DReader(payload)
     gmr = OrderedDict()
 
     gmr['schema_version'] = rd.read_uint16()
-    gmr['game_version'] = rd.read_uint16();
+    gmr['game_version'] = rd.read_uint16()
     gmr['game_code'] = rd.read_uint16()
-    gmr['game_speed'] = gmr['game_code'] & (1 << 0)
-    gmr['game_diff'] = gmr['game_code'] & (1 << 1)
-    gmr['escape_time'] = rd.read_fixed32();
+    gmr['game_speed'] = True if gmr['game_code'] & (1 << 1) else False
+    gmr['game_diff'] = True if gmr['game_code'] & (1 << 0) else False
+    gmr['escape_time'] = round(rd.read_fixed32(), 2)
     gmr['escaped'] = gmr['escape_time'] > 0.0
-    gmr['challenges_completed'] = rd.read_uint8();
-    gmr['challenges_total'] = rd.read_uint8();
+    rd.read_uint8()
+    gmr['challenges_completed'] = 0
+    gmr['challenges_total'] = rd.read_uint8()
 
     gmr['players'] = OrderedDict()
     for i in range(1, MAX_PLAYERS + 1):
@@ -140,11 +140,12 @@ def decode_game_result(payload):
         completed_by = rd.read_uint8()
         if not completed_by:
             continue
+        gmr['challenges_completed'] += 1
 
         gmr['challenges'][i] = OrderedDict()
         gmr['challenges'][i]['completed_by'] = completed_by
-        gmr['challenges'][i]['completed_time'] = rd.read_fixed32();
-        gmr['challenges'][i]['order'] = rd.read_uint8();
+        gmr['challenges'][i]['completed_time'] = round(rd.read_fixed32(), 2)
+        gmr['challenges'][i]['order'] = rd.read_uint8()
 
         gmr['challenges'][i]['buttons_by'] = OrderedDict()
         for l in range(0, CHALLENGE_BUTTON_MAX):
@@ -154,11 +155,75 @@ def decode_game_result(payload):
         for l in range(0, CHALLENGE_POWERUP_MAX):
             gmr['challenges'][i]['powerups_by'][l] = rd.read_uint8()
 
-    print(json.dumps(
-        gmr,
-        indent=4
-    ))
-    print('%d / %d' % (rd.offset, len(rd.buff)))
+    return gmr
+
+
+PLAYER_TYPE_MAP = [
+    'FREE',
+    'NONE',
+    'USER',
+    'COMPUTER',
+    'NEUTRAL',
+    'HOSTILE',
+]
+
+GAME_SPEED_MAP = [
+    'SLOWER',   # 0
+    'SLOW',     # 1
+    'NORMAL',   # 2
+    'FAST',     # 3
+    'FASTER',   # 4
+]
+
+
+class GeneralSection(OrderedDict):
+    def __init__(self):
+        OrderedDict.__init__(self)
+        self.setdefault('game_title', None)
+        self.setdefault('game_speed', None)
+        self.setdefault('elapsed_game_loops', None)
+        self.setdefault('elapsed_game_time', None)
+        self.setdefault('elapsed_real_time', None)
+        self.setdefault('player_slots', [])
+
+    def addMetadata(self, metadata):
+        for row in metadata['Players']:
+            self['player_slots'].append(OrderedDict(
+                player_id=row['PlayerID'],
+                apm=row['APM'],
+            ))
+
+    def addHeader(self, header):
+        self['elapsed_game_loops'] = header['m_elapsedGameLoops']
+
+    def addDetails(self, details):
+        self['game_title'] = details['m_title']
+        self['game_speed'] = GAME_SPEED_MAP[details['m_gameSpeed']]
+        self['elapsed_game_time'] = round(self['elapsed_game_loops'] * 16.0, 2)
+        self['elapsed_real_time'] = round(self['elapsed_game_time'] * (1 + ((details['m_gameSpeed'] - 2) / 5.0)), 2)
+
+        for row in details['m_playerList']:
+            pslot = self['player_slots'][row['m_workingSetSlotId']]
+            pslot['type'] = PLAYER_TYPE_MAP[row['m_control']]
+            if row['m_control'] == 2:
+                pslot['handle'] = '%d-S2-%d-%d' % (row['m_toon']['m_region'], row['m_toon']['m_realm'], row['m_toon']['m_id'])
+            else:
+                pslot['handle'] = None
+                pslot['name'] = row['m_name']
+            pslot['color'] = OrderedDict()
+            pslot['color']['r'] = row['m_color']['m_r']
+            pslot['color']['g'] = row['m_color']['m_g']
+            pslot['color']['b'] = row['m_color']['m_b']
+            pslot['color']['a'] = row['m_color']['m_a']
+
+    def addInitData(self, initd):
+        for i, row in enumerate(initd['m_syncLobbyState']['m_userInitialData']):
+            if i >= len(self['player_slots']):
+                break
+            pslot = self['player_slots'][i]
+            if row['m_name']:
+                pslot['name'] = row['m_name']
+            pslot['clan'] = row['m_clanTag'] if row['m_clanTag'] else None
 
 
 def main():
@@ -184,10 +249,35 @@ def main():
         protocol = versions.latest()
         print('Attempting to use newest possible instead: %s' % protocol.__name__, file=sys.stderr)
 
-    contents = read_contents(archive, 'replay.tracker.events')
-    tevents = protocol.decode_replay_tracker_events(contents)
+    metadata = json.loads(read_contents(archive, 'replay.gamemetadata.json'))
+    details = protocol.decode_replay_details(read_contents(archive, 'replay.details'))
+    initd = protocol.decode_replay_initdata(read_contents(archive, 'replay.initData'))
 
-    map(decode_game_result, fetch_payloads_from_tracker(tevents))
+    general = GeneralSection()
+    game_result = None
+    
+    general.addMetadata(metadata)
+    general.addHeader(header)
+    general.addDetails(details)
+    general.addInitData(initd)
+
+    tevents = protocol.decode_replay_tracker_events(read_contents(archive, 'replay.tracker.events'))
+
+    CV_NAMES = [
+        'Ice Baneling Escape - Cold Voyage',
+        'Ice Baneling Escape - EZ',
+        'Ice Baneling Escape - Pro',
+    ]
+    if general['game_title'] in CV_NAMES:
+        for payload in fetch_payloads_from_tracker(tevents):
+            game_result = decode_game_result(payload)
+            if game_result['escaped']:
+                break
+
+    osects = OrderedDict()
+    osects['general'] = general
+    osects['result'] = game_result
+    print(json.dumps(osects, indent=4, sort_keys=False))
 
 
 if __name__ == '__main__':
